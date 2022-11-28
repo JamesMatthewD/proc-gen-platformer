9130b4bc7e498be69709458223fa8f68af541ed1
James <111821718+JamesMatthewD@users.noreply.github.com>
2022-11-28 12:15:43 +0000

    Better skeleton Frame

--- classes.py
+++ classes.py
@@ -6,7 +6,7 @@ vec=pygame.math.Vector2
 
 class MovingEntity(pygame.sprite.Sprite):
 
-    def __init__(self, health, posX, posY, damageNum, typeOfEntity):
+    def __init__(self, health, posX, posY, damageNum):
 
         super().__init__()
         self.pos=vec(posX, posY)
@@ -16,20 +16,25 @@ class MovingEntity(pygame.sprite.Sprite):
         self.damage=damageNum
 
         self.jumping=False
-        self.type=typeOfEntity
 
 
-    def move(self):
-        if self.type==0:
-            ##PLAYER
-            pass
-        elif self.type==1:
-            ##ENEMY
-            pass
+    def move(self, moveType):
+        if moveType=='Left':
+            ##Process left movement
+        elif moveType=='Right':
+            ##Process right movement
+        elif moveType=='Jump':
+            ##Call jump method//continue jump
+        elif moveType=='Attack':
+            ##Proccess standard attack
+        elif moveType=='Special Attack':
+            ##Proccess the special attack, wouldn't be called by enemies
+        else:
+            ##Stop acceleration or jump and check colissions
 
 
-    def update(self):
-        ##Collisions and other stuff
+    def checkCollisions(self, objectGroup, otherGroup, False):
+        ##Checks the collisions
         pass
 
     def jump(self):
@@ -40,7 +45,7 @@ class MovingEntity(pygame.sprite.Sprite):
         ##if key is let go then stop upward acceleration
         pass
 
-    def damageTaken(self):
+    def damageTaken(self, damageNumber):
         ##Call in update to change the health
         pass
 
@@ -53,13 +58,20 @@ class MovingEntity(pygame.sprite.Sprite):
 class Player(MovingEntity):
 
     def __init__(self, health, posX, posY, damageNum):
-        super().__init__(health, posX, posY, damageNum, 0)
+        super().__init__(health, posX, posY, damageNum)
 
         self.score=0
         self.battleBar=0.0
 
+    def update(self, objectGroup, otherGroup):
+        super().checkCollisions(objectGroup, otherGroup)
+        pass
+        #Would then perform actions based on the collisions
+        #Would also add next frame to animation queue.
+        
     def heal(self):
         ##HEAL PLAYER, call after update method
+        super().damageTaken() #Use negative number for the parameter, negative damage = heal
         pass
 
     def getAttackType(self):
@@ -83,10 +95,16 @@ class Player(MovingEntity):
 class Enemy(MovingEntity):
 
     def __init__(self, health, posX, posY, damageNum):
-        super().__init__(health, posX, posY, damageNum, 1)
+        super().__init__(health, posX, posY, damageNum)
+        
+    
+    def update(self, objectGroup, otherGroup):
+        super().checkCollisions(objectGroup, otherGroup)
+        pass ##Perform actions based on the collisions
+        #If collides with the hitbox for player attack, then would call the super damage taken method.
 
     def pathfind(self):
-        ##do pathfinding algorithm etc
+        ##do pathfinding algorithm and call move method.
         pass
 
 
@@ -107,7 +125,8 @@ class HUD(pygame.sprite.Sprite):
 
 class Level():
     def __init__(self):
-        self.level=[[],[],[],[],[],[],[],[],[]]
+        self.level=[[],[],[],[],[],[],[],[]] #8 Rows of blocks, 1 row for ceiling, 1 row for floor, I don't currently know how many columns there will be
+        #The level generation will use characters in the arrays for each row, and will generate the correct sprite/the correct object
 
     def generate(self):
         for eachRow in self.level:
