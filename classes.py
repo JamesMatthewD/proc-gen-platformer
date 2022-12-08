4764a69341578815a27a1fc6ac7c5ee118d963e8
James <111821718+JamesMatthewD@users.noreply.github.com>
2022-12-08 08:18:35 +0000

    V0.1

--- classes.py
+++ classes.py
@@ -131,9 +131,98 @@ class HUD(pygame.sprite.Sprite):
 
 class Level():
     def __init__(self):
-        self.level=[[],[],[],[],[],[],[],[]] #8 Rows of blocks, 1 row for ceiling, 1 row for floor, I don't currently know how many columns there will be
-        #The level generation will use characters in the arrays for each row, and will generate the correct sprite/the correct object
+        self.level=[]
+        
+        myFile=open('initialLevel.txt', 'r')
+        for eachRow in myFile:
+            row=[]
+            chars=eachRow
+            for eachChar in chars:
+                if eachChar!='\n':
+                    row.append(eachChar)
 
+            self.level.append(row)
+
+        self.elements={'air': 0,
+                       'block': 1,
+                       'enemy': 2}  #This dictionary doesn't contain entrance and exit as they won't be changeable locations and thus wouldn't be worth including in here
+
+        self.immutables={'air': 'a',
+                         'block': 'b',
+                         'enemy': 'c',
+                         'entrance': 'd',
+                         'exit': 'e'}
+
+            
+        #8 Rows of blocks, 1 row for ceiling, 1 row for floor, I don't currently know how many columns there will be
+        #The level generation will use characters in the arrays for each row, and will generate the correct sprite/the correct object
+    
     def generate(self):
+
+        ##Entrance exit generation
+        entranceLocation=random.randint(1,6)
+        exitLocation=random.randint(1,6)
+        self.level[entranceLocation][0]=self.immutables['entrance']
+        self.level[entranceLocation+1][1]=self.immutables['block']
+        self.level[entranceLocation][1]=self.immutables['air']
+        self.level[exitLocation][9]=self.immutables['exit']
+        self.level[exitLocation+1][8]=self.immutables['block']  #There is a block next to the entrance and exit so that the player can enter them easily
+        self.level[exitLocation][8]=self.immutables['air'] #There is air next to the entrance and exit so that they aren't blocked off
+
+        
+        row=0
         for eachRow in self.level:
-            pass  #Generate level 1 row at a time
+            col=0
+            for eachCol in eachRow:
+                try:
+                    test=int(self.level[row][col])%1 #This makes is so that an error is caused from attempting to divide a letter so it knows to skip that block
+                    if row==0 or row==7 or col==0 or col==9:
+                        self.level[row][col]=self.immutables['block']  #This makes it so that borders of the level are blocks (prevents player falling out of level
+                        col+=1
+                        continue
+
+                    chosenElement=random.random()  #This generates a random number that I can use to determine how to level will be generated
+
+                    if chosenElement>0.95:
+                        self.level[row][col]=self.elements['enemy']
+                        
+                        if row!=7:
+                            self.level[row+1][col]=self.immutables['block']
+
+                            if col==1:
+                                self.level[row+1][col+1]=self.immutables['block']
+                                self.level[row][col+1]=self.immutables['air']
+                                
+                            elif col==8:
+                                self.level[row+1][col-1]=self.immutables['block']
+                                self.level[row][col-1]=self.immutables['air']
+                                
+                            else:
+                                leftRight=random.random()
+                                
+                                if leftRight>0.5:
+                                    self.level[row+1][col-1]=self.immutables['block']
+                                    self.level[row][col-1]=self.immutables['air']
+                                    
+                                else:
+                                    self.level[row+1][col+1]=self.immutables['block']
+                                    self.level[row][col+1]=self.immutables['air']
+
+                    elif chosenElement>0.65:
+                        self.level[row][col]=self.elements['block']
+
+                    else:
+                        self.level[row][col]=self.elements['air']
+
+                    col=col+1
+                except:
+                    col+=1
+            row+=1
+            
+
+
+##Debugging/Testing
+level=Level()
+level.generate()
+for eachRow in level.level:
+    print(eachRow)
